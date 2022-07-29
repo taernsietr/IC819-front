296240e06944d48c21e080294676d80c5ed0ab71
Gabriel Phelippe Bezerra <gabrielpbezerra8@gmail.com>
2022-07-29 13:59:20 -0300

    testando json pq o thalles pediu

--- src/front/components/Menu/Menu.tsx
+++ src/front/components/Menu/Menu.tsx
@@ -7,20 +7,22 @@ import searchSvg from '../../assets/svg/search.svg';
 import arrowSvg from '../../assets/svg/arrow.svg';
 import cartSvg from '../../assets/svg/cart.svg'
 import profileSvg from '../../assets/svg/profile.svg';
+import menuItems from './test.json'
 
 function Menu() {
-
+    let menuItems = [
+        { "name": "Marmitas fit" },
+        { "name": "Massas" }
+      ];
     return (
         <div className='Menu'>
             <header className='menu'>
                 <img id='menuIcon' src={menuLogo} alt='icon'/>
-
                 {/* Fazer um menuDropDown com useState para renderizar os cardápios cadastrados */}
                 <button onClick={() => {}} className='menuDropDown'>
                 <p>Pratos clássicos</p> 
                         <img src={arrowSvg} alt=''/>
                 </button>
-
                 <div className='menuSearch'>
                     <img src={searchSvg} />
                     <input placeholder='Buscar cardápio ou prato...' type='text'/>   
