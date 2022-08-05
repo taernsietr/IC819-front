471f5a8244464ddd6c39ce11b292ee95bc905454
Gabriel Phelippe Bezerra <gabrielpbezerra8@gmail.com>
2022-08-04 21:05:05 -0300

    using boostrap and axios for rendering menu dropdown

--- src/front/components/DropDownMenus/DropDownMenu.tsx
+++ src/front/components/DropDownMenus/DropDownMenu.tsx
@@ -1,31 +1,53 @@
 import React from 'react';
+import { useEffect, useState } from "react";
 import Dropdown from 'react-bootstrap/Dropdown';
 import './DropDownMenu.css';
 import arrowSvg from '../../assets/svg/arrow.svg';
+import axios from 'axios';
+import { get } from 'https';
+
+const customAxios = axios.create({ baseURL: "http://localhost:5000/"});
+
+
 
 function DropDownMenu() {
-    let menuItemsData = [
-        { "name": "Marmitas fit" },
-        { "name": "Massas" },
-        { "name": "Marmitas gourmet" },
-        { "name": "Nhoque" },
-      ];
+    const [object, setObject] = useState([ {
+        name: ""
+        }
+    ]);
+    
+    useEffect( () => {
+        const getMenuNames = async () => {
+            try {
+                const response = await customAxios.get("/menuItemsData");
+                if (response.data != null) {
+                    console.log(response.data)
+                    setObject(response.data);
+                }
+            } catch(error) {
+                console.log(error);
+            };
+        };
+        getMenuNames();
+    }); 
+
 
     return (
-        <Dropdown className="dropDownComponent">
-            <Dropdown.Toggle id="dropDownButton">
-                <p>Cardápios</p>
-                <img src={arrowSvg} alt="" id='arrowSvg'/>
+        <Dropdown >
+            <Dropdown.Toggle className="dropDrownButton">
+                <p id="firstMenu">{object[0].name}</p>
             </Dropdown.Toggle>
-
-            <Dropdown.Menu className='dropDownDeck'>
-                {menuItemsData.map((res, index)=> {
-                    return (
-                    <Dropdown.Item id="dropDownItem" href="">{res.name}</Dropdown.Item>
-                )})}
+            <Dropdown.Menu>
+                {object.shift();
+                object.map((res,key) => {
+                    return(
+                        <Dropdown.Item key={key} href="/">{res.name}</Dropdown.Item>
+                    )
+                })}
             </Dropdown.Menu>
         </Dropdown>
     );
 }
+
 
 export default DropDownMenu;
