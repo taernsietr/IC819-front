7b9cd5609ea5a0e3422e262a54dcfd2970f66c7f
Gabriel Phelippe Bezerra <gabrielpbezerra8@gmail.com>
2022-08-05 00:08:40 -0300

    fix containerMenu naming

--- src/front/components/ContainerMenus/ContainerMenus.tsx
+++ src/front/components/ContainerMenus/ContainerMenus.tsx
@@ -1,36 +1,11 @@
 import React from 'react';
 import { useEffect, useState } from "react";
-import './ContainerMenu.css';
+import './ContainerMenus.css';
 import axios from 'axios';
 
 const customAxios = axios.create({ baseURL: "http://192.168.3.14:5000/"});
 
 const ContainerMenus = () => {
-
-    
-function DropDownMenu() {
-    const [object, setObject] = useState([ {
-        name: ""
-        }
-    ]);
-    
-    useEffect( () => {
-        const getMenuNames = async () => {
-            try {
-                const response = await customAxios.get("/menuItemsData");
-                if (response.data != null) {
-                    console.log(response.data)
-                    setObject(response.data);
-                }
-            } catch(error) {
-                console.log(error);
-            };
-        };
-
-        getMenuNames();
-    }); 
-
-
     return(
         <div className="menuContainer">
         <h1 id="menuName">Pratos classicos</h1>
@@ -51,4 +26,4 @@ function DropDownMenu() {
     )
 }
 
-export default ContainerMenus
+export default ContainerMenus;
