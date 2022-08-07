23c65b6890bab1a6b9d602b4243d27722813e64b
Isabelle Rizzo <isabelle965rizzo@gmail.com>
2022-08-07 18:30:57 -0300

    atualizando eslint

--- src/back/db.ts
+++ src/back/db.ts
@@ -1,8 +1,5 @@
 // referência: https://github.com/mariadb-developers/todo-app-nodejs-sequelize/blob/main/src/api/db.js
 import "../../dotenv/config";
-
-// TODO: desativar essa regra do eslint
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
