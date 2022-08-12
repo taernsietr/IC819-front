108b63d0d4a5da97e32fb17ad60ee4be2ff03a75
Isabelle Rizzo <isabelle965rizzo@gmail.com>
2022-08-12 12:04:31 -0300

    add arquivos de validação (precisa refatorar)

---
+++ src/back/resources/index.js
@@ -0,0 +1,16 @@
+
+import cpfValidation from "./validations/cpfValidation";
+import emailValidation from "./validations/emailValidation";
+import phoneValidation from "./validations/phoneValidation";
+
+import generateAccessToken from "./validations/generateAccessToken";
+
+
+const resources = {
+	emailValidation,
+	phoneValidation,
+	cpfValidation,
+	generateAccessToken,
+};
+
+export default resources;
---
+++ src/back/resources/validations/phoneValidation.js
@@ -0,0 +1,29 @@
+/* Função para validar o telefone */
+
+export default function phoneValidation(phone) {
+  if (!phone || phone == '') {
+    return new Error({
+      message: 'Telefone inválido.'
+    });
+  } else {
+    //tirar letras e tal
+    phone = phone.toString().replace(/\D/g,'');
+
+    if (phone.length < 10 ||
+      phone == '00000000000' ||
+      phone == '11111111111' ||
+      phone == '22222222222' ||
+      phone == '33333333333' ||
+      phone == '44444444444' ||
+      phone == '55555555555' ||
+      phone == '66666666666' ||
+      phone == '77777777777' ||
+      phone == '88888888888' ||
+      phone == '99999999999'
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+}
