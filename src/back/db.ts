b27b21654635643c238cf799d11e952bdfaa5f25
Thalles Rodrigues <ts.rodr@gmail.com>
2022-08-17 20:59:45 -0300

    linted db.ts

--- src/back/db.ts
+++ src/back/db.ts
@@ -2,25 +2,17 @@
 import "../../dotenv/config";
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DB_NAME,
-				process.env.DB_USER,
-				process.env.DB_PASS, {
-					host: process.env.DB_HOST,
-					port: process.env.DB_PORT,
-					dialect: "mariadb",
-					});
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, { host: process.env.DB_HOST, port: process.env.DB_PORT, dialect: "mariadb", });
 
-async function batatinha() {
-	try {
-		await sequelize
-			.authenticate()
-			.then(() => {
-				console.log('Connection authenticated successfully');
-			})
-			.catch((error: any) => { throw error; });
-	} catch(error) { throw error; }	
-};
+async function authenticateSequelize() {
+	await sequelize
+		.authenticate()
+		.then(() => {
+			console.log("Connection authenticated successfully");
+		})
+		.catch((error: any) => { throw error; });
+}
 
-batatinha();
+authenticateSequelize();
 
 export default sequelize;
