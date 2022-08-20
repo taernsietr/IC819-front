c7fdc88114208398245b932bf3ebed0799b00d08
Thalles Rodrigues <ts.rodr@gmail.com>
2022-08-19 22:54:41 -0300

    funções prontas, dados mais ou menos gerados

---
+++ exercicio-02.js
@@ -0,0 +1,15 @@
+/*
+2. Escrever uma função que esconda parte dos endereços de e-mail para proteger os dados dos
+usuários
+Exemplo entrada: meuemail@gmail.com
+Exemplo saída: meu****@gmail.com
+*/
+
+function obfuscateEmail(email) {
+    match = email.match(/(\w+)(\@\w+\.\w+)/);
+    split = Math.floor(match[1].length/2);
+    
+    return match[1].slice(0, split) + match[1].slice(split).replace(/\w/g, "*") + match[2];
+}
+
+export default obfuscateEmail;
